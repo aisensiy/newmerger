e16d29218eb85a3d4e7336ffedf68b925a041bde
aisensiy <aisensiy@gmail.com>
2015-01-06 23:02:44 +0800

    add seed to import csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
+
 User.delete_all
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Bargain.delete_all
+bargain_rows = CSV.read('db/bargains.csv', headers: true)
+bargain_rows.each { |row| Bargain.create!(row.to_hash) }
+
+Buyer.delete_all
+buyer_rows = CSV.read('db/buyers.csv', headers: true)
+buyer_rows.each { |row| Buyer.create!(row.to_hash) }
+
+Target.delete_all
+target_rows = CSV.read('db/targets.csv', headers: true)
+target_rows.each { |row| Target.create(row.to_hash) }
