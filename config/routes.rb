77e8fcec347dde50caa9654f252111f25dee92f3
aisensiy <aisensiy@gmail.com>
2015-01-13 19:40:14 +0800

    add shell companies view

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post 'target_finder/show_attrs'
   post 'target_finder/show_results'
 
+  get 'chance_finder/get_shell_companies'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
