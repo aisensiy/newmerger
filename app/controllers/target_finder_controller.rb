7f7288c296f240ce3ac982ce08b8a7ca802ad322
aisensiy <aisensiy@gmail.com>
2015-01-13 10:00:15 +0800

    add buyer attr form

--- app/controllers/target_finder_controller.rb
+++ app/controllers/target_finder_controller.rb
@@ -1,2 +1,9 @@
 class TargetFinderController < ApplicationController
+  def index
+    @industries = Industry.where(is_deprecated: false,
+                                 is_secondary: false).includes(:targets, :buyers)
+    @industries = @industries.select do |industry|
+      industry.targets.size > 0 && industry.buyers.size > 0
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post 'buyer_finder/show_attrs'
   post 'buyer_finder/show_results'
 
+  get 'target_finder/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
